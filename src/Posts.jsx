d285322c15bb169fb6c1d57d118c508818da16b2
mdmegha1999 <94183187+mdmegha1999@users.noreply.github.com>
2023-07-10 22:15:10 +0530

    Update Posts.jsx

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,38 +1,48 @@
-import React, {useState, useEffect} from 'react';
-import Card from './Card';
-
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import './Card.css'
 export default function Posts() {
-    const [post, setPost]= useState([]);
-    const [search, setSearch] = useState("");
-    function getData(){
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res)=>res.json())
-        .then((data)=>{
-            let posts = [...data]
-            if(post==""){
-                setPost(posts)
-            }
+  const [post, setPost] = useState([]);
+  const [search, setSearch] = useState("");
 
-        });
-    }
-    getData()
-    const inputHandler = (e)=>{
-        let searchval = e.target.value.toLowerCase();
-        setSearch(searchval);
-    }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        let posts = [...data];
+        setPost(posts);
+      });
+  }
+
+  const inputHandler = (e) => {
+    let searchVal = e.target.value.toLowerCase();
+    setSearch(searchVal);
+  };
+
+  const searchHandler = () => {
+    const filteredPosts = post.filter((item) =>
+      item.title.toLowerCase().includes(search)
+    );
+    setPost(filteredPosts);
+  };
+
   return (
     <div>
       <div className="header">
-      
-      <input placeholder="Seacth" onChange={inputHandler}/>
+        <input placeholder="Search" onChange={inputHandler} />
+        <button type="submit" className="btn" onClick={searchHandler}>
+          <i className="fa fa-search"></i>
+        </button>
       </div>
       <div className="card-ul">
-      {post.filter((item,i)=>item.title.includes(search)).map((item,i)=>
-    <Card key={item.id} title={item.title} body={item.body}/>
-    )}
-    <br></br>
-    <h2>Completed</h2>
+        {post.map((item) => (
+          <Card key={item.id} title={item.title} body={item.body} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
